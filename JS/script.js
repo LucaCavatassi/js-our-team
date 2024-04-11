78228c9b373be557e62244a47fd95257b831d052
LucaCavatassi <lucamariacavatassi@gmail.com>
2024-04-11 14:44:10 +0200

    fix: missing-image name in objects

--- JS/script.js
+++ JS/script.js
@@ -3,31 +3,38 @@ let teamMembers = [
         name: "Wayne",
         surname: "Barnett",
         role: "Founder & CEO",
+        image: "wayne-barnett-founder-ceo.jpg",
     },
     {
         name: "Angela",
         surname: "Caroll",
         role:  "Chief & Editor",
+        image: "angela-caroll-chief-editor.jpg",
     },
     {
         name: "Walter",
         surname: "Gordon",
         role: "Office Manager",
+        image: "walter-gordon-office-manager.jpg",
+
     },
     {
         name: "Angela",
         surname: "Lopez",
         role: "Social Media Manager",
+        image: "angela-lopez-social-media-manager.jpg",
     },
     {
         name: "Scott",
         surname: "Estrada",
         role: "Developer",
+        image: "scott-estrada-developer.jpg",
     },
     {
         name:  "Barbara",
         surname: "Ramos",
         role: "Graphic Designer",
+        image: "barbara-ramos-graphic-designer.jpg",
     },
 ];
 
